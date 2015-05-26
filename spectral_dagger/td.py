d6c459bea6e0a1eea2a37aa40d56e596141a300b
Eric Crawford <e2crawfo@uwaterloo.ca>
2015-05-26 14:08:12 -0400

    TD-method improvements.
    * Added a the pure TD-method for learning value functions on markov
      chains (so policy evaluation, but no control).
    
    * Added a method to schedule change in both epsilon and alpha over time.
    
    * Added eligibility traces to both TD and Sarsa, so both can accept a
      parameter L (lambda).
    
    * Added test for pure TD-method.

--- spectral_dagger/td.py
+++ spectral_dagger/td.py
@@ -3,16 +3,86 @@ from mdp import MDPPolicy
 
 
 class TD(MDPPolicy):
-    """ A base class for TD policies."""
+    """ A prediction policy. Learns the value function of a policy
+    acting on an MDP, but does not execute actions."""
 
-    def __init__(self):
-        raise NotImplemented("Cannot instantiate abstract base class `TD`.")
+    def __init__(self, mdp, alpha, L=0, V_0=None):
+        self.states = mdp.states
+        self.actions = mdp.actions
+        self.gamma = mdp.gamma
 
-    def update_epsilon(self):
-        if self.epsilon_schedule:
+        self.alpha = alpha
+        self.L = L
+
+        if L > 0:
+            self.eligibility_trace = np.zeros(mdp.n_states)
+
+        self.active_traces = []
+
+        if V_0 is None:
+            V_0 = 100 * np.ones(mdp.n_states)
+
+        self.V = V_0.copy()
+
+    def reset(self, state):
+        self.current_state = state
+
+        if self.L > 0:
+            self.eligibility_trace[:] = 0
+            self.active_traces = []
+
+    def update(self, action, state, reward=None):
+        prev_state = self.current_state
+
+        if self.L > 0:
+            self.eligibility_trace[prev_state] += 1
+            if prev_state not in self.active_traces:
+                self.active_traces.append(prev_state)
+
+        if reward is not None:
+            V = self.V
+            delta = reward + self.gamma * V[state] - V[prev_state]
+
+            if self.L > 0:
+                for s in self.active_traces:
+                    et = self.eligibility_trace[s]
+                    V[s] += self.alpha * delta * et
+            else:
+                V[prev_state] += self.alpha * delta
+
+        self.current_state = state
+
+        for s in self.active_traces[:]:
+            self.eligibility_trace[s] *= self.gamma * self.L
+
+            if self.eligibility_trace[s] < 0.001:
+                self.active_traces.remove(s)
+
+
+class ControlTD(TD):
+    """
+    A base class for TD policies that include control. Chooses actions
+    epsilon-greedily with respect to current estimate of action-value
+    function (i.e. Q(s, a)).
+
+    """
+
+    def __init__(self, mc, alpha):
+        raise NotImplemented(
+            "Cannot instantiate abstract base class `ControlTD`.")
+
+    def update_parameters(self):
+        if hasattr(self._epsilon, 'next'):
             try:
-                next_epsilon = self.epsilon_schedule.next()
+                next_epsilon = self._epsilon.next()
                 self.epsilon = next_epsilon
+            except StopIteration:
+                pass
+
+        if hasattr(self._alpha, 'next'):
+            try:
+                next_alpha = self._alpha.next()
+                self.alpha = next_alpha
             except StopIteration:
                 pass
 
@@ -27,10 +97,9 @@ class TD(MDPPolicy):
                 key=lambda a: self.Q[self.current_state, a])
 
 
-class QLearning(TD):
+class QLearning(ControlTD):
     def __init__(
-            self, mdp, alpha, epsilon=0.1,
-            epsilon_schedule=None, Q_0=None):
+            self, mdp, alpha, epsilon=0.1, Q_0=None):
 
         self.states = mdp.states
         self.actions = mdp.actions
@@ -40,15 +109,22 @@ class QLearning(TD):
 
         if Q_0 is None:
             Q_0 = 100 * np.ones((mdp.n_states, mdp.n_actions))
-        self.Q = Q_0
+        self.Q = Q_0.copy()
 
-        self.epsilon = epsilon
-        self.epsilon_schedule = epsilon_schedule
-        self.update_epsilon()
+        self._epsilon = epsilon
+        self._alpha = alpha
+
+        if not hasattr(self._epsilon, 'next'):
+            self.epsilon = self._epsilon
+
+        if not hasattr(self._alpha, 'next'):
+            self.alpha = self._alpha
+
+        self.update_parameters()
 
     def reset(self, state):
         self.current_state = state
-        self.update_epsilon()
+        self.update_parameters()
 
     def update(self, action, state, reward=None):
         prev_state = self.current_state
@@ -62,44 +138,88 @@ class QLearning(TD):
         self.current_state = state
 
 
-class Sarsa(TD):
+class Sarsa(ControlTD):
     def __init__(
-            self, mdp, alpha, epsilon=0.1,
-            epsilon_schedule=None, Q_0=None):
+            self, mdp, alpha, L=0, epsilon=0.1, Q_0=None):
+        """
+        L is the lambda parameter for TD sarsa. L = 0 equivalent to
+        vanilla SARSA
+        """
 
         self.states = mdp.states
         self.actions = mdp.actions
         self.gamma = mdp.gamma
 
-        self.alpha = alpha
+        self.L = L
+
+        # list of lists of length 2. 1st item is (s, a) pair,
+        # second item is eligibility trace value
+        if L > 0:
+            self.eligibility_trace = np.zeros((mdp.n_states, mdp.n_actions))
+
+        self.active_traces = []
 
         if Q_0 is None:
-            Q_0 = 100 * np.ones((mdp.n_states, mdp.n_actions))
-        self.Q = Q_0
+            Q_0 = 1 * np.ones((mdp.n_states, mdp.n_actions))
 
-        self.epsilon = epsilon
-        self.epsilon_schedule = epsilon_schedule
-        self.update_epsilon()
+        self.Q = Q_0.copy()
+
+        self._epsilon = epsilon
+        self._alpha = alpha
+
+        if not hasattr(self._epsilon, 'next'):
+            self.epsilon = self._epsilon
+
+        if not hasattr(self._alpha, 'next'):
+            self.alpha = self._alpha
+
+        self.update_parameters()
 
     def reset(self, state):
         self.prev_state = None
         self.prev_action = None
         self.prev_reward = None
 
+        if self.L > 0:
+            self.eligibility_trace[:] = 0
+            self.active_traces = []
+
         self.current_state = state
 
-        self.update_epsilon()
+        self.update_parameters()
 
     def update(self, action, state, reward=None):
 
         if self.prev_reward is not None:
+            if self.L > 0:
+                self.eligibility_trace[self.prev_state, self.prev_action] += 1
+                s, a = (self.prev_state, self.prev_action)
+                if (s, a) not in self.active_traces:
+                    self.active_traces.append((s, a))
+
             Q = self.Q
-            Q[self.prev_state, self.prev_action] += self.alpha * (
+
+            delta = (
                 self.prev_reward + self.gamma * Q[self.current_state, action]
                 - Q[self.prev_state, self.prev_action])
+
+            if self.L > 0:
+                for (s, a) in self.active_traces:
+                    et = self.eligibility_trace[s, a]
+                    Q[s, a] += self.alpha * delta * et
+            else:
+                Q[self.prev_state, self.prev_action] += self.alpha * delta
 
         self.prev_state = self.current_state
         self.prev_action = action
         self.prev_reward = reward
 
         self.current_state = state
+
+        for s, a in self.active_traces[:]:
+            self.eligibility_trace[s, a] *= self.gamma * self.L
+
+            if self.eligibility_trace[s, a] < 0.001:
+                self.active_traces.remove((s, a))
+
+        # self.eligibility_trace *= self.gamma * self.L
--- spectral_dagger/tests/test_td.py
+++ spectral_dagger/tests/test_td.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from spectral_dagger.td import QLearning, Sarsa
+from spectral_dagger.td import QLearning, Sarsa, TD
 from spectral_dagger.grid_world import GridWorld
+from spectral_dagger.random_walk import RandomWalk
 
 cliff_world = np.array([
     ['x', 'x', 'x', 'x', 'x', 'x'],
-    ['x', 'A', ' ', ' ', 'O', 'x'],
+    ['x', 'S', ' ', ' ', 'O', 'x'],
     ['x', ' ', ' ', ' ', 'O', 'x'],
     ['x', ' ', ' ', ' ', 'O', 'x'],
     ['x', ' ', ' ', ' ', 'G', 'x'],
@@ -17,7 +18,7 @@ canyon_world = np.array([
     ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
     ['x', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
     ['x', ' ', ' ', 'O', 'O', 'O', ' ', ' ', 'x'],
-    ['x', 'A', ' ', ' ', ' ', 'O', 'G', ' ', 'x'],
+    ['x', 'S', ' ', ' ', ' ', 'O', 'G', ' ', 'x'],
     ['x', ' ', ' ', 'O', 'O', 'O', ' ', ' ', 'x'],
     ['x', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
     ['x', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
@@ -26,21 +27,50 @@ canyon_world = np.array([
 noise = 0.1
 gamma = 0.9
 epsilon = 0.1
-alpha = 0.01
-n_episodes = 10000
+alpha = [0.1, 0.2]
+lmbda = [0, 0.3, 0.4, 1]
+n_episodes = 100
 
 
 def pytest_generate_tests(metafunc):
-    if "algorithm" in metafunc.funcargnames:
-        metafunc.parametrize(
-            "algorithm", [QLearning, Sarsa])
+    if "alpha" in metafunc.funcargnames:
+        metafunc.parametrize("alpha", alpha)
+
+    if "lmbda" in metafunc.funcargnames:
+        metafunc.parametrize("lmbda", lmbda)
 
 
-def test_td(algorithm, display=False):
+def test_td_prediction(alpha, lmbda, display=False):
+    env = RandomWalk(10, p=0.5, gamma=1.0)
+
+    policy = TD(
+        env, alpha=alpha, L=lmbda,
+        V_0=np.random.random(env.n_states))
+
+    env.sample_trajectory(
+        policy, reset=True, display=display)
+    env.sample_trajectory(
+        policy, reset=True, display=display)
+
+
+def test_q_learning(alpha, display=False):
     env = GridWorld(cliff_world, noise=noise, gamma=gamma)
 
-    policy = algorithm(
+    policy = QLearning(
         env, alpha=alpha, epsilon=epsilon,
+        Q_0=np.random.random((env.n_states, env.n_actions)))
+
+    env.sample_trajectory(
+        policy, reset=True, display=display)
+    env.sample_trajectory(
+        policy, reset=True, display=display)
+
+
+def test_sarsa(alpha, lmbda, display=False):
+    env = GridWorld(cliff_world, noise=noise, gamma=gamma)
+
+    policy = Sarsa(
+        env, alpha=alpha, L=lmbda, epsilon=epsilon,
         Q_0=np.random.random((env.n_states, env.n_actions)))
 
     env.sample_trajectory(
