34499c609bfe2999ac3687e1dff7feaaca04894a
Eric Crawford <eric.crawford@mail.mcgill.ca>
2016-07-13 10:48:02 -0400

    Made DataExperiment use sklearn RandomSearchCV.
    * Previously had my our own class for doing this, but RandomSearchCV has
      some benefits like easy multiprocessing.

--- spectral_dagger/utils/experiment.py
+++ spectral_dagger/utils/experiment.py
@@ -6,17 +6,97 @@ import logging
 import abc
 import six
 import sklearn
-from sklearn.cross_validation import train_test_split, KFold
+
+from scipy.stats import uniform
+
+from sklearn.cross_validation import train_test_split
 from sklearn.metrics import make_scorer
+from sklearn.grid_search import RandomizedSearchCV
 
 import spectral_dagger as sd
-from spectral_dagger.utils.cv import Dataset, CrossValidator, Estimator
 
 pp = pprint.PrettyPrinter()
 verbosity = 2
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+
+def update_attrs(obj, attrs):
+    for k, v in six.iteritems(attrs):
+        setattr(obj, k, v)
+
+
+class Estimator(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractproperty
+    def record_attrs(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def point_distribution(self, context, rng):
+        raise NotImplementedError()
+
+
+class Dataset(object):
+    """ A generic dataset.
+
+    Parameters/Attributes
+    ---------------------
+    X: any
+        Input data.
+    y: any
+        Output data.
+
+    """
+    def __init__(self, X, y):
+        if len(X) == 1:
+            X = [X]
+        self.X = X
+
+        if len(y) == 1:
+            y = [y]
+        self.y = y
+
+        assert len(self.X) == len(self.y)
+
+    def __len__(self):
+        return len(self.X)
+
+    def __str__(self):
+        return "<Dataset. len=%d>" % len(self)
+
+    def __repr__(self):
+        return str(self)
+
+    def __iter__(self):
+        return [(x, y) for x, y in zip(self.X, self.y)]
+
+    def __getitem__(self, key):
+        return Dataset(self.X[key], self.y[key])
+
+
+class UnsupervisedDataset(Dataset):
+    """ A generic dataset.
+
+    Parameters/Attributes
+    ---------------------
+    X: any
+        Data.
+
+    """
+    def __init__(self, X):
+        if len(X) == 1:
+            X = [X]
+        self.X = X
+        self.y = None
+
+    def __iter__(self):
+        return [x for x in self.X]
+
+    def __getitem__(self, key):
+        return UnsupervisedDataset(self.X[key])
 
 
 class Experiment(object):
@@ -39,8 +119,6 @@ class DataExperiment(object):
         ----------
         base_estimators: Estimator class or list of Estimator classes.
             The base_estimators to be tested.
-        model_selector: ModelSelector instance
-            An object for selecting hypotheses.
         x_var_name: string
             Name to use for the x variable (aka independent variable).
         x_var_values: list-like
@@ -61,13 +139,16 @@ class DataExperiment(object):
             Number of samples to take for each value of the x variable.
         seed: int or RandomState
             Random state for the experiment.
-        data_kwargs: dict
+        data_kwargs: dict (optional)
             Key word arguments for the data generation function.
+        search_kwargs: dict (optional)
+            Key word arguments for the call to RandomizedSearchCV.
 
     """
     def __init__(
-            self, base_estimators, model_selector, x_var_name, x_var_values,
-            generate_data, score, n_repeats=5, seed=1, data_kwargs=None):
+            self, base_estimators, x_var_name, x_var_values,
+            generate_data, score, n_repeats=5, seed=1, data_kwargs=None,
+            search_kwargs=None):
 
         self.base_estimators = base_estimators
 
@@ -96,13 +177,16 @@ class DataExperiment(object):
             self.score_names = ['score']
 
         self.score = score[0]
-
         self.n_repeats = n_repeats
-        self.model_selector = model_selector
-
         self.seed = seed
-
         self.data_kwargs = {} if data_kwargs is None else data_kwargs
+
+        self.search_kwargs = dict(
+            n_iter=10, n_jobs=4, cv=None, iid=False,
+            error_score=np.inf, random_state=seed,
+            pre_dispatch='n_jobs')
+        if search_kwargs is not None:
+            self.search_kwargs.update(search_kwargs)
 
     def run(self):
         print("Running experiment.")
@@ -120,7 +204,6 @@ class DataExperiment(object):
                     {self.x_var_name: x, 'seed': data_seed})
 
                 train, test = self.generate_data(**data_kwargs)
-                self.model_selector.add_training_data(train)
 
                 for base_est in self.base_estimators:
                     est = sklearn.base.clone(base_est)
@@ -132,8 +215,13 @@ class DataExperiment(object):
                         "" % (base_est.__class__.__name__, self.x_var_name,
                               str(x), i, est.random_state))
 
-                    learned_est = (
-                        self.model_selector.select(est, self.scores[0]))
+                    dists = est.point_distribution(
+                        context=data_kwargs, rng=rng)
+                    search = RandomizedSearchCV(
+                        est, dists, scoring=self.scores[0],
+                        **self.search_kwargs)
+                    search.fit(train.X, train.y)
+                    learned_est = search.best_estimator_
 
                     print("    Best parameter setting:"
                           " %s" % learned_est.get_params())
@@ -148,7 +236,14 @@ class DataExperiment(object):
                         results[-1][sn] = score
 
                     for attr in learned_est.record_attrs:
-                        results[-1][attr] = getattr(learned_est, attr)
+                        value = getattr(learned_est, attr)
+                        try:
+                            value = float(value)
+                        except ValueError:
+                            pass
+
+                        results[-1][attr] = value
+                        print("    Value for attr %s: %s" % (attr, value))
 
         self.results = results
         self.df = pd.DataFrame.from_records(results)
@@ -157,8 +252,8 @@ class DataExperiment(object):
 
 
 if __name__ == "__main__":
-    """ Explore how lasso and ridge regression perform as the
-    amount of data available to them changes. """
+    """ Explore how lasso and ridge regression performance changes
+    as the amount of training data changes. """
 
     import matplotlib.pyplot as plt
     from sklearn import datasets, linear_model
@@ -177,27 +272,41 @@ if __name__ == "__main__":
     mae_score = make_scorer(
         sklearn.metrics.mean_absolute_error, greater_is_better=False)
 
+    class LogUniform(object):
+        def __init__(self, low, high, base=np.e, random_state=None):
+            assert low < high
+            self.low = low
+            self.high = high
+            self.uniform = uniform(loc=low, scale=high-low)
+            self.uniform.random_state = random_state
+            self.base = base
+
+        def rvs(self, size=1):
+            return self.base ** self.uniform.rvs(size)
+
     class Ridge(linear_model.Ridge, Estimator):
         record_attrs = ['alpha']
 
-        def _generate_points(self, max_evals, context, rng):
-            return [dict(alpha=a) for a in np.logspace(-4, -.5, 30)]
+        def point_grid(self, context, rng):
+            return {'alpha': np.logspace(-4, -.5, 30)}
+
+        def point_distribution(self, context, rng):
+            return {'alpha': LogUniform(-4, -.5, 10, rng)}
 
     class Lasso(linear_model.Lasso, Estimator):
         record_attrs = ['alpha']
 
-        def _generate_points(self, max_evals, context, rng):
-            return [dict(alpha=a) for a in np.logspace(-4, -.5, 30)]
+        def point_grid(self, context, rng):
+            return {'alpha': np.logspace(-4, -.5, 30)}
 
-    def f(n_points):
-        return KFold(n_points, n_folds=10, shuffle=False)
-
-    selector = CrossValidator(f, n_jobs=1)
+        def point_distribution(self, context, rng):
+            return {'alpha': LogUniform(-4, -.5, 10, rng)}
 
     experiment = DataExperiment(
         [Ridge(0.1), Lasso(0.1)],
-        selector, 'train_size', np.linspace(0.1, 0.8, 8),
-        generate_diabetes_data, [(mse_score, 'NMSE'), (mae_score, 'NMAE')])
+        'train_size', np.linspace(0.1, 0.8, 8),
+        generate_diabetes_data, [(mse_score, 'NMSE'), (mae_score, 'NMAE')],
+        search_kwargs=dict(cv=10, n_jobs=2))
     df = experiment.run()
 
     plot_measures(df, ['NMSE', 'NMAE', 'alpha'], 'train_size', 'method')
